c867c017a0d99a894748503b9749dc37bc0a25d3
priyansh2120 <priyanshkumar50@gmail.com>
2024-03-04 03:44:52 +0530

    removed blog and added github

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -142,14 +142,14 @@ function Navbar() {
 
                         <Fade left>
                             <Link
-                                href='/#blog'
+                                href='https://github.com/priyansh2120/'
                             >
                                 <div className="my-[2em] mx-auto rounded-[78.8418px] text-[#1D9BF0] bg-[#15202B] sm:w-[85%] w-[100%] h-[55px] sm:h-[60px] flex items-center justify-evenly px-[25px] sm:px-[30px] box-border border-2 border-[#1D9BF0] hover:text-[#15202B] hover:bg-[#1D9BF0] transition-colors">
                                     <FaFolderOpen
                                         className="text-xl sm:text-2xl"
                                     />
                                     <span className="w-6/12 text-[1.125rem] sm:text-[1.3rem] font-semibold">
-                                        Blog
+                                        Github
                                     </span>
                                 </div>
                             </Link>
