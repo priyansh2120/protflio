e1b6d1bcfaa293e6ce9f3f389bb95e448b4bb6a1
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-10-28 14:08:15 +0530

    Update socials-data.js

--- src/data/socials-data.js
+++ src/data/socials-data.js
@@ -2,8 +2,8 @@ export const socialsData = {
   mail: "priyanshkumar50@gmail.com",
   github: "https://github.com/priyansh2120",
   facebook: "https://www.facebook.com/priyansh.aryap/",
-  linkedIn: "https://www.linkedin.com/in/priyansh-kumar-296983224/",
-  twitter: "https://twitter.com/ConsciousBucker",
+  linkedIn: "https://www.linkedin.com/in/priyansh2120",
+  twitter: "https://x.com/kaptaan_amrikaa",
   medium: "",
   stackOverflow: "",
 };
