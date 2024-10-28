68bea19b37e8f91880e5e45d87cf1b9ea30b2a8f
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-10-28 13:06:42 +0530

    Update about-data.js

--- src/data/about-data.js
+++ src/data/about-data.js
@@ -1,8 +1,8 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "I am Priyansh. I'm a Full Stack web Developer, a DevOps Enthusiast and Problem solving Enthusiast as well.",
+    "I am Priyansh, a Full Stack Developer, DevOps enthusiast, and problem solver.",
   description2:
-    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes React.js, Node.js, Express.js, Next.js, MongoDB, MySQL, Tailwind CSS, Bootstrap, HTML5, CSS, Figma(Basic), Canva(Basic), VS Code, Version Control: Git/Github.  Linux OS, Cloud: AWS, Containerization: Docker, Kubernetes , Infrastructure as Code: Ansible, CI/CD: Jenkins, Monitoring : Prometheus",
+    "A quick learner with a passion for exploring new technologies, I am committed to creating impactful solutions. My current stack includes React.js, Node.js, Express.js, Next.js, MongoDB, MySQL, Tailwind CSS, Bootstrap, and DevOps tools like Docker, Kubernetes, Jenkins, and Ansible. I’m also skilled in Git, AWS, Linux OS, and monitoring tools like Prometheus.",
   image: 1,
 };
