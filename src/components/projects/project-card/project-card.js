7cca34ba6ab9a2d3c1495a161b77faff1373e321
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-10-28 16:26:20 +0530

    Update project-card.js

--- src/components/projects/project-card/project-card.js
+++ src/components/projects/project-card/project-card.js
@@ -6,8 +6,6 @@ import placeholder from '../../../assets/png/placeholder.png';
 import styles from '../../../styles/singleProject.module.css';
 
 function SingleProject({ id, name, desc, tags, code, demo, image, theme }) {
-
-
     return (
         <Fade bottom>
             <div
@@ -28,17 +26,15 @@ function SingleProject({ id, name, desc, tags, code, demo, image, theme }) {
                             href={demo}
                             target='_blank'
                             rel='noreferrer'
-                            className="flex items-center justify-center w-[40px] h-[40px] rounded-[50px] border-2 border-[#EFF3F4] hover:border-[#15202B] text-[#EFF3F4] hover:text-[#1D9BF0] transition hover:scale-[1.1]"
-                            aria-labelledby={`${name
-                                .replace(' ', '-')
-                                .toLowerCase()} ${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-demo`}
+                            className="flex items-center justify-center w-[40px] h-[40px] rounded-[50px] border-2 transition hover:scale-[1.1]"
+                            style={{
+                                color: theme.buttonColor,
+                                borderColor: theme.buttonColor,
+                            }}
+                            aria-labelledby={`${name.replace(' ', '-').toLowerCase()} ${name.replace(' ', '-').toLowerCase()}-demo`}
                         >
                             <FaPlay
-                                id={`${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-demo`}
+                                id={`${name.replace(' ', '-').toLowerCase()}-demo`}
                                 className="text-[1.1rem] transition"
                                 aria-label='Demo'
                             />
@@ -47,17 +43,15 @@ function SingleProject({ id, name, desc, tags, code, demo, image, theme }) {
                             href={code}
                             target='_blank'
                             rel='noreferrer'
-                            className="flex items-center justify-center w-[40px] h-[40px] rounded-[50px] border-2 border-[#EFF3F4] hover:border-[#15202B] text-[#EFF3F4] hover:text-[#1D9BF0] transition hover:scale-[1.1]"
-                            aria-labelledby={`${name
-                                .replace(' ', '-')
-                                .toLowerCase()} ${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-code`}
+                            className="flex items-center justify-center w-[40px] h-[40px] rounded-[50px] border-2 transition hover:scale-[1.1]"
+                            style={{
+                                color: theme.buttonColor,
+                                borderColor: theme.buttonColor,
+                            }}
+                            aria-labelledby={`${name.replace(' ', '-').toLowerCase()} ${name.replace(' ', '-').toLowerCase()}-code`}
                         >
                             <FaCode
-                                id={`${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-code`}
+                                id={`${name.replace(' ', '-').toLowerCase()}-code`}
                                 className="text-[1.1rem] transition"
                                 aria-label='Code'
                             />
