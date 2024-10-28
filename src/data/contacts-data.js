983a140fc2c2b91a6c59d7b61640f24987361452
priyansh2120 <priyanshkumar50@gmail.com>
2024-10-28 14:08:35 +0530

    Update contacts-data.js

--- src/data/contacts-data.js
+++ src/data/contacts-data.js
@@ -5,7 +5,7 @@ export const contactsData = {
   github: "https://github.com/priyansh2120",
   facebook: "https://www.facebook.com/priyansh.aryap/",
   linkedIn: "https://www.linkedin.com/in/priyansh2120/",
-  twitter: "https://twitter.com/kaptaanamrikaa",
+  twitter: "https://x.com/kaptaan_amrikaa",
   medium: "",
   stackOverflow: "",
 };
