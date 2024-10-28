44a8fdb88502a255d5b72ec7245250c26f583a97
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-10-28 13:39:04 +0530

    Update projects-data.js

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,3 +1,11 @@
+import eight from "../assets/svg/projects/eight.svg";
+import five from "../assets/svg/projects/five.svg";
+import four from "../assets/svg/projects/four.svg";
+import one from "../assets/svg/projects/one.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import six from "../assets/svg/projects/six.svg";
+import three from "../assets/svg/projects/three.svg";
+import two from "../assets/svg/projects/two.svg";
 export const projectsData = [
   {
     id: 2,
