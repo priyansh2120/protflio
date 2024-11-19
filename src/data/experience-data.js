be903e5cd323d7b61767e43e03bdc4bbe9d1142e
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-11-20 00:53:57 +0530

    Update experience-data.js

--- src/data/experience-data.js
+++ src/data/experience-data.js
@@ -1,27 +1,34 @@
 export const experienceData = [
   {
     id: 1,
+    company: "Bizz Plus",
+    jobtitle: "Full Stack Developer Intern",
+    startYear: "Nov 2024",
+    endYear: "Present",
+  },
+  {
+    id: 2,
     company: "Fractal Analytics",
     jobtitle: "Project Intern",
     startYear: "May 2024",
     endYear: "Oct 2024",
   },
   {
-    id: 2,
+    id: 3,
     company: "Metastart",
     jobtitle: "Full Stack Developer Intern",
     startYear: "Mar 2024",
     endYear: "May 2024",
   },
   {
-    id: 3,
+    id: 4,
     company: "E-Cell IIIT Pune",
     jobtitle: "Lead Web Developer",
     startYear: "Jan 2022",
     endYear: "Present",
   },
   {
-    id: 4,
+    id: 5,
     company: "Code Clause",
     jobtitle: "Web Development Intern Trainee",
     startYear: "May 2023",
