fc73573f51e93679f80c965c424dc39eb0866533
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2025-03-02 15:53:15 +0530

    Update resume link

--- src/data/header-data.js
+++ src/data/header-data.js
@@ -6,5 +6,5 @@ export const headerData = {
   image: "https://i.ibb.co/4mBb77L/Priyansh-NA-Task1.jpg",
   imagebw: "https://i.ibb.co/4mBb77L/Priyansh-NA-Task1.jpg",
   resumePdf:
-    "https://drive.google.com/file/d/1b1xQVE2-5YJ1y13oQTuGSTJwVPTBpmQf/view?usp=drive_link",
+    "https://drive.google.com/file/d/1o3wmwhCLKHk9JF4EuZxutpn44VboMQvF/view?usp=sharing",
 };
